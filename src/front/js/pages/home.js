62d606df07c14453c12b960f565d8de51f702495
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-11-03 20:58:45 +0000

    started headline of home page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,35 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-		
-		</div>
-	);
+  return (
+    <main className="text-center mt-5">
+      <section className="bg-yellow">
+        <nav className="navbar navbar-light bg-light">
+          <form className="form-inline d-flex">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </nav>
+
+        {/*Headline*/}
+
+        <div className="d-flex">
+          {/*TEXT LEFT*/}
+          <div> </div>
+          {/* FOTO RIGHT */}
+          <div> </div>
+        </div>
+      </section>
+    </main>
+  );
 };
