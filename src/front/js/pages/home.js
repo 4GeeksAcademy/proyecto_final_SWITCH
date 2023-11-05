686aa9848285acdf0d4591609180520e24330a5d
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-11-05 19:50:45 +0000

    [search bar and explanation of switch in the home page] - I am working on dividing the search bar into activity and location, so the search can be more complex (like other websites usually do) - The pic of the banner, is a placeholder and must be replaced.

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,33 +1,73 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import "../../styles/home.css";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <main className="text-center mt-5">
-      <section className="bg-yellow">
-        <nav className="navbar navbar-light bg-light">
-          <form className="form-inline d-flex">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">
-              Search
+    <main className=" bg-yellow">
+      <section className="bg-yellow d-flex flex-column container pt-5 ">
+        {/* <div className="d-flex justify-start pt-5">
+          <h3 className="fw-bold extradark-grey font-nunito">
+            Encuentra tu <span className="dark-blue">grupo </span> perfecto.
+          </h3>{" "}
+        </div> */}
+
+        {/*SEARCH BAR */}
+        <div className="p-2 bg-light position-relative rounded-3 shadow">
+          <form className="form-inline d-flex  ">
+            <div className="flex-grow-1 form-group me-2">
+              <input
+                className="form-control mr-sm-2  py-2 px-4 text-16 bg-light input-activity "
+                type="search"
+                placeholder="Tipo de actividad, tempos..."
+                aria-label="Search"
+              />{" "}
+            </div>
+            <div className="form-group flex-grow-1 me-5 ">
+              <input
+                className="form-control  py-2 px-4 text-16 bg-light input-activity "
+                type="text"
+                placeholder="Location"
+              />
+            </div>
+            <button
+              className="btn rounded-3 input-btn bg-dark-blue fw-bolder text-16 text-light my-2 my-sm-0 px-4"
+              type="submit"
+            >
+              Encuentra tu grupo
             </button>
           </form>
-        </nav>
+        </div>
 
         {/*Headline*/}
 
-        <div className="d-flex">
+        <div className="d-flex justify-between pt-5 pb-5">
           {/*TEXT LEFT*/}
-          <div> </div>
+          <div className="pe-5">
+            <h2 className="fw-bold text-52 extradark-grey font-nunito">
+              Qué es <span className="dark-blue">Switch </span>
+            </h2>
+            <p className="extradark-grey fs-4 pt-3 font-nunito">
+              Switch es una app intutitiva donde personas reales hablan y
+              enseñan idiomas, a la vez que se apoyan, crean nuevas amistades,
+              exploran lugares, descubren el arte, la naturaleza o la
+              gastronomía del mundo.{" "}
+            </p>
+            <p className="extradark-grey fs-4 font-nunito">
+              Nuestra metodología consiste en conectar a personas en actividades
+              divertidas mientras mejoran sus habilidades lingüísticas y a
+              facilitar a los organizadores la gestión de sus eventos y grupos.{" "}
+            </p>
+          </div>
           {/* FOTO RIGHT */}
-          <div> </div>
+          <div>
+            <img
+              src="https://placehold.co/600x400"
+              alt="people having a conversation"
+            />
+          </div>
         </div>
       </section>
     </main>
