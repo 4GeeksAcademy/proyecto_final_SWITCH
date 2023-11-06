f41e69c67aa6851a65481dcb8b75f02737f04b74
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-11-06 17:49:38 +0000

    Tabla de 'Group' creado en 'models.py'

--- src/api/models.py
+++ src/api/models.py
@@ -30,3 +30,31 @@ class User(db.Model):
             "photo_url": self.photo_url,
             "is_active": self.active            
         }
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+    id_group = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    id_organizer = db.Column(db.Integer, db.ForeignKey('user.id_user'))
+    organizer = db.relationship(User)
+    city = db.Column(db.String(50), unique=False, nullable=False)
+    languages = db.Column(db.String(100), unique=False, nullable=False)
+    description = db.Column(db.String(300), nullable=False)  
+    photo_url = db.Column(db.String(500), unique=False, nullable=False)
+    # member_count = db.Column(db.Integer) cómo incorporamos esto aquí?
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Group: {self.name}, Organizer: {self.organizer}>'
+
+    def serialize(self):
+        return {
+            "id": self.id_user,
+            "group": self.name, 
+            "organizer": self.id_organizer,
+            "city": self.city,
+            "languages": self.languages,
+            "description": self.description,
+            "photo_url": self.photo_url,
+            "is_active": self.active            
+        }
