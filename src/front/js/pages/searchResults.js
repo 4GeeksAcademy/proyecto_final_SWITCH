cbe7be0133a2e0ebb84ca8face9231e1cd5239c7
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-11-07 18:43:25 +0000

    Concepto inicial para la página 'Search Results'

--- src/front/js/pages/searchResults.js
+++ src/front/js/pages/searchResults.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
 export const SearchResults = () => {
 	const { store, actions } = useContext(Context);
@@ -32,6 +33,41 @@ export const SearchResults = () => {
 				Encuentra tu grupo
 				</button>
 			</form>
+		</div>
+
+		{/* RESULT BOXES */}
+		{/* Tendremos que producir estas contendores desde una función map() 
+		    cuando tengamos unos grupos en la BBDD 'Groups'. De momento estos
+			contendores son tipo 'placeholder' */}
+		<div className="container mt-4 bg-warning rounded-3 mx-sm-1 mx-md-5 fs-5 fw-bold">
+			<div className="row">
+				<div className="col text-left p-3">
+					<span className="bg-white p-2 rounded-3">Group Name</span>
+				</div>
+				<div className="col text-end p-3">
+					<Link to={'/GroupPageLink/{uid}'} className="bg-white p-2 rounded-3 searchLink">View Group</Link>
+				</div>
+			</div>
+		</div>
+		<div className="container mt-4 bg-warning rounded-3 mx-sm-1 mx-md-5 fs-5 fw-bold">
+			<div className="row">
+				<div className="col text-left p-3">
+					<span className="bg-white p-2 rounded-3">Group Name</span>
+				</div>
+				<div className="col text-end p-3">
+					<Link to={'/GroupPageLink/{uid}'} className="bg-white p-2 rounded-3 searchLink">View Group</Link>
+				</div>
+			</div>
+		</div>
+		<div className="container mt-4 bg-warning rounded-3 mx-sm-1 mx-md-5 fs-5 fw-bold">
+			<div className="row">
+				<div className="col text-left p-3">
+					<span className="bg-white p-2 rounded-3">Group Name</span>
+				</div>
+				<div className="col text-end p-3">
+					<Link to={'/GroupPageLink/{uid}'} className="bg-white p-2 rounded-3 searchLink">View Group</Link>
+				</div>
+			</div>
 		</div>
 		</>
 		
