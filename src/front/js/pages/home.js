1e334613a3ef464a76ac1e5d8db51e7eed2f7bef
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-11-07 17:22:30 +0000

    [search bar and Headline in the home page, without responsiveness yet]     - search bar divided into activity and location and with icons.     - Headline text improved.     - The pic of the banner, is a placeholder and must be replaced.

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,38 +2,59 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
+import locationIcon from "../../img/home-location-icon.png";
+import searchIcon from "../../img/home-search-icon.png";
+
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
     <main className=" bg-yellow">
       <section className="bg-yellow d-flex flex-column container pt-5 ">
-        {/* <div className="d-flex justify-start pt-5">
-          <h3 className="fw-bold extradark-grey font-nunito">
-            Encuentra tu <span className="dark-blue">grupo </span> perfecto.
-          </h3>{" "}
-        </div> */}
-
         {/*SEARCH BAR */}
-        <div className="p-2 bg-light position-relative rounded-3 shadow">
-          <form className="form-inline d-flex  ">
-            <div className="flex-grow-1 form-group me-2">
+
+        <div className=" bg-light position-relative rounded-3 home-search-bar-shadow over overflow-hidden">
+          {/*FORM */}
+
+          <form className="form-inline d-flex justify-content-between ">
+            {/*SEARCH SIDE */}
+
+            <div className="d-flex flex-grow-1">
+              {/*SEARCH ICON */}
+
+              <div className="bg-dark-blue p-2 px-3 rounded">
+                <img src={searchIcon} height="40px" className="home-icons" />
+              </div>
+
+              {/*SEARCH INPUT */}
               <input
-                className="form-control mr-sm-2  py-2 px-4 text-16 bg-light input-activity "
+                className=" mr-sm-2  py-2 px-4 text-16 bg-light home-inputs w-100 "
                 type="search"
                 placeholder="Tipo de actividad, tempos..."
-                aria-label="Search"
-              />{" "}
+              />
             </div>
-            <div className="form-group flex-grow-1 me-5 ">
+
+            {/*LOCATION SIDE */}
+
+            <div className="d-flex flex-grow-1">
+              {/*LOCATION ICON */}
+
+              <div className="bg-dark-blue py-2 px-3 rounded">
+                <img src={locationIcon} height="40px" className="home-icons" />
+              </div>
+              {/*LOCATION INPUT */}
+
               <input
-                className="form-control  py-2 px-4 text-16 bg-light input-activity "
-                type="text"
+                className=" py-2 px-4 text-16 bg-light home-inputs w-100"
+                type="search"
                 placeholder="Location"
               />
             </div>
+
+            {/*SUBMIT BUTTON */}
+
             <button
-              className="btn rounded-3 input-btn bg-dark-blue fw-bolder text-16 text-light my-2 my-sm-0 px-4"
+              className="btn rounded-3 rounded-end home-input-btn bg-dark-blue fw-bolder text-16 text-light my-2 my-sm-0 px-4"
               type="submit"
             >
               Encuentra tu grupo
@@ -43,28 +64,33 @@ export const Home = () => {
 
         {/*Headline*/}
 
-        <div className="d-flex justify-between pt-5 pb-5">
+        <div className="d-flex justify-between  headline">
           {/*TEXT LEFT*/}
           <div className="pe-5">
-            <h2 className="fw-bold text-52 extradark-grey font-nunito">
+            <h2 className="fw-bold text-72 extradark-grey font-nunito">
               Qué es <span className="dark-blue">Switch </span>
             </h2>
-            <p className="extradark-grey fs-4 pt-3 font-nunito">
+            <p className="extradark-grey fs-4 pt-3 font-nunito pe-5">
               Switch es una app intutitiva donde personas reales hablan y
               enseñan idiomas, a la vez que se apoyan, crean nuevas amistades,
               exploran lugares, descubren el arte, la naturaleza o la
               gastronomía del mundo.{" "}
             </p>
-            <p className="extradark-grey fs-4 font-nunito">
+            <p className="extradark-grey fs-4 font-nunito pe-5 pt-4">
               Nuestra metodología consiste en conectar a personas en actividades
               divertidas mientras mejoran sus habilidades lingüísticas y a
               facilitar a los organizadores la gestión de sus eventos y grupos.{" "}
             </p>
+
+            <button className="btn rounded-3 rounded home-input-btn bg-dark-blue fw-bolder text-16 text-light px-5 py-3 mt-4">
+              Únete ya
+            </button>
           </div>
+
           {/* FOTO RIGHT */}
           <div>
             <img
-              src="https://placehold.co/600x400"
+              src="https://placehold.co/600x500"
               alt="people having a conversation"
             />
           </div>
