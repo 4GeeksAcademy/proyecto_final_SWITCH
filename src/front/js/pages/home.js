3476ca8e92c4ce798520d43db9cf87171252cbbf
Vanesa Juarez Paris <131259155+vanesascode@users.noreply.github.com>
2023-11-09 20:24:13 +0000

    Home headline and home explanation section finished and responsive

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,12 +3,32 @@ import "../../styles/home.css";
 
 import { HomeHeadline } from "../component/HomeHeadline";
 import { HomeSearchBar } from "../component/HomeSearchBar";
+import { HomeExplanation } from "../component/HomeExplanation";
 
 export const Home = () => {
   return (
-    <main className=" bg-yellow">
-      <HomeSearchBar />
-      <HomeHeadline />
-    </main>
+    <>
+      {/* SEARCH BAR*/}
+
+      <div className=" bg-yellow">
+        <section className=" d-flex flex-column container pt-5 ">
+          <HomeSearchBar />
+        </section>
+
+        {/* HEADLINE*/}
+
+        <section className=" container  ">
+          <HomeHeadline />
+        </section>
+      </div>
+
+      {/* EXPLANATION OF APP*/}
+
+      <div className=" bg-light">
+        <section className=" container  ">
+          <HomeExplanation />
+        </section>
+      </div>
+    </>
   );
 };
