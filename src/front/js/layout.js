06cea4b505957f2f810d22500f8d7b0b04aa70cb
marthaklie <138883071+marthaklie@users.noreply.github.com>
2023-11-09 17:32:15 +0000

    creada versión responsive de la página Group Page

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import GroupPage from "./pages/GroupPage";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -27,7 +28,8 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<GroupPage />} path="/GroupPage" />
+                        <Route element={<Single />} path="/single/:theid" />                        
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
