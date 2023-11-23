9116981bc6bb523ff51f305a1d53acecd49b8834
David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-11-23 17:53:03 +0000

    Prueba 2 de Render.com

--- src/front/js/component/FormCreateUser.js
+++ src/front/js/component/FormCreateUser.js
@@ -139,8 +139,6 @@ export const FormCreateUser = () => {
     }
   }, [store.userCreatedFailure])
 
-
-  console.log("TESSSSSSSSSSSSSSSSSTING")
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // COMPONENT
 
